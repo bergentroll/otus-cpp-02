c5ae18a2ecf49c16e05b26ab894080b86737b49b
Anton Karmanov <bergertroll@insiberia.net>
2020-05-09 17:39:53 +0500

    Revert "Exterminate"
    This reverts commit c509f8d2bf5b0c3b297281796438794ce98e3242.

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "ipv4_address.h"
+#include "helpers.h"
+
+using namespace otus;
+
+void printIPv4Vector(const std::vector<IPv4Address> &vec)
+{
+    for (auto &addr: vec)
+    {
+        std::cout << addr << std::endl;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    std::vector<IPv4Address> ip_pool;
+
+    try
+    {
+        for (std::string line; std::getline(std::cin, line);)
+        {
+            auto v = split(line, '\t');
+            ip_pool.push_back(IPv4Address(v.at(0)));
+        }
+    }
+    catch (IPv4Address::InvalidAddressString &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::sort(ip_pool.rbegin(), ip_pool.rend());
+    printIPv4Vector(ip_pool);
+
+    auto result = filter(ip_pool, 1);
+    printIPv4Vector(result);
+
+    result = filter(ip_pool, 46, 70);
+    printIPv4Vector(result);
+
+    result = filter_any(ip_pool, 46);
+    printIPv4Vector(result);
+
+    return 0;
+}
